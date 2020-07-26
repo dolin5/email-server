39f31b1b2f79ac21093f29c6597aaa578ab6492b
ng911datasync <dylan@olingeographics.com>
2020-07-26 14:21:24 -0600

    modified email sending

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')
 let auth;
 
-if(process.env.NODE_ENV === 'production'){
-    auth = process.env;
+if (process.env.NODE_ENV === 'production') {
+  auth = process.env;
 }
-else{
-    auth = require('./config.json');
+else {
+  auth = require('./config.json');
 }
 
 const transporter = nodemailer.createTransport({
@@ -17,44 +17,43 @@ const transporter = nodemailer.createTransport({
     user: auth.SENDGRID_USERNAME, pass: auth.SENDGRID_PASSWORD
   }
 });
-
 const app = express();
-app.use(cors())
-
+app.use(cors());
 app.use(bodyParser.json({}));
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
-app.post('/sendEmail', (req, res)=>{
-    let email = req.body.email;
-    let message = req.body.message;
-    let name = req.body.name;
-    let gif = req.body.gif;
-
+app.post('/sendEmail', (req, res) => {
+  let email = req.body.email;
+  let message = req.body.message;
+  let name = req.body.name;
+  let gif = req.body.gif;
+  if (['http://localhost:8080','https://ng911datasync.com'].indexOf(req.headers.origin)>=0){
     transporter.sendMail({
-        from: email,
-        to: 'olindyla@gmail.com',
-        subject: `${name}` ,
-        html: `<h3>${message}</h3>`
-        }, (err, info)=>{
-            if(err){
-                res.send(err);
-            }
-            else{
-                console.log('after info');
-                res.status(200).json({
-                success: true,
-                message: 'Email Sent'
-                });
-            }
+      from: email,
+      to: 'dylan@olingeographics.com',
+      subject: `NG911 Data Sync Message`,
+      html: `<span>${message}</span><br><span>${name}</span>`
+    }, (err, info) => {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        console.log('after info');
+        res.status(200).json({
+          success: true,
+          message: 'Email Sent'
         });
+      }
+    });
+  }
 });
 
-app.listen(PORT, ()=>{
-    console.log('Listening on port: ' + PORT);
+app.listen(PORT, () => {
+  console.log('Listening on port: ' + PORT);
 });
 
